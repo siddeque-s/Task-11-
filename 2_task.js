38ad0bebf0d2a1fb441013214a888d3854c33595
Siddeque Sajeev <siddeques45@gmail.com>
2025-08-12 18:04:28 +0530

    question2 completed

---
+++ 2_task.js
@@ -0,0 +1,12 @@
+// 5         51025
+// 6         61236
+// 7         71449
+
+const promt = require("prompt-sync")();
+let num = promt("Enter number");
+
+let result = "";
+
+result = "" + num * 1 + num * 2 + num ** 2;
+
+console.log(result);
